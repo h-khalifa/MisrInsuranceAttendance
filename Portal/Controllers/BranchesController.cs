d20234db46c05aa5122a9b800c931c3caac33258
haitham <haitham_khalifa@outlook.com>
2022-10-06 19:01:38 +0200

    branches module

---
+++ Portal/Controllers/BranchesController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DataAceess.Interfaces;
+using DataAceess.Models;
+
+namespace Portal.Controllers
+{
+    public class BranchesController : Controller
+    {
+        private IUnitOfWork _db;
+
+        public BranchesController(IUnitOfWork db)
+        {
+            _db = db;
+        }
+
+
+        public ActionResult Index()
+        {
+            var branches = _db.BranchesRepo.GetAll();
+            return View(branches);
+        }
+
+        // GET: Branches/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Branch branch = _db.BranchesRepo.GetById(id.Value);
+            if (branch == null)
+            {
+                return HttpNotFound();
+            }
+            return View(branch);
+        }
+
+        // GET: Branches/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Branches/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Name,Desc")] Branch branch)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.BranchesRepo.Add(branch);
+                _db.Submit();
+                return RedirectToAction("Index");
+            }
+
+            return View(branch);
+        }
+
+        // GET: Branches/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Branch branch = _db.BranchesRepo.GetById(id.Value);
+            if (branch == null)
+            {
+                return HttpNotFound();
+            }
+            return View(branch);
+        }
+
+        // POST: Branches/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name,Desc")] Branch branch)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.BranchesRepo.Edit(branch);
+                _db.Submit();
+                return RedirectToAction("Index");
+            }
+            return View(branch);
+        }
+
+        // GET: Branches/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Branch branch = _db.BranchesRepo.GetById(id.Value);
+            if (branch == null)
+            {
+                return HttpNotFound();
+            }
+            return View(branch);
+        }
+
+        // POST: Branches/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Branch branch = _db.BranchesRepo.GetById(id);
+            _db.BranchesRepo.Delete(branch);
+            _db.Submit();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
